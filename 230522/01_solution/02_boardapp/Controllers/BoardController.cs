208675c746802fee1a9e5a4af8b6a35d589229b0
drowned <fight1999club@gmail.com>
2023-05-22 17:27:07 +0900

    230522

---
+++ 230522/01_solution/02_boardapp/Controllers/BoardController.cs
@@ -0,0 +1,38 @@
+﻿using _02_boardapp.Data;
+using _02_boardapp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _02_boardapp.Controllers
+{
+    // https://localhost:7800/Board/Index
+    public class BoardController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public BoardController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Board> objBoardList = _db.Boards.ToList();  // Select query
+            return View(objBoardList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Board board) 
+        {
+            _db.Boards.Add(board);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index", "Board");
+        }
+    }
+}
